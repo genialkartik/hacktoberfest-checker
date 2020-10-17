46e30bb74951fe40b77987dcb1d067935ee28e3a
genialkartik <kartik9756@gmail.com>
2020-10-17 23:53:45 +0530

    api done

--- routes/index.js
+++ routes/index.js
@@ -2,62 +2,63 @@ const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
+const axios = require('axios')
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  // res.render('index', {
-  //   title: 'Express',
-  //   date: new Date(Date.parse('2020-10-13T18:27:05Z')).toUTCString()
-  // });
-  fetch("https://api.github.com/repos/genialkartik/Blog-Socketio/topics",
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/vnd.github.mercy-preview+json"
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.name)
-      var _t = data.name.find((e => e == 'hacktoberfest'))
-      res.send(_t ? true : false)
-    })
+let ar_PR = [];
+
+router.get('/', (req, res) => {
+  var clientId = 'c55d4f3e951937805749'
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}`);
 });
 
-// https://api.github.com/search/issues?q=author%3Agenialkartik+type%3Apr
-async function getTopics(repo_url) {
-  const user_repo_res = await fetch(repo_url + '/topics',
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/vnd.github.mercy-preview+json"
-      }
-    })
-  const user_data = await user_repo_res.json()
-  var _topic = user_data.name.find(e => e == 'hacktoberfest')
-  return (_topic ? true : false)
-}
+router.get('/login', (req, res) => {
+  const body = {
+    client_id: 'c55d4f3e951937805749',
+    client_secret: 'afd3d3cc7e82d501dca6033e6eee231da82900ab',
+    code: req.query.code
+  };
+  const opts = { headers: { accept: 'application/json' } };
+  axios.post(`https://github.com/login/oauth/access_token`, body, opts).
+    then(res => res.data['access_token']).
+    then(_token => {
+      console.log('My token:', _token);
+      res.json({ ok: 1 });
+    }).
+    catch(err => res.status(500).json({ message: err.message }));
+});
 
 async function getPRs(callback) {
   try {
-    let ar_PR = [];
     const response = await fetch('https://api.github.com/search/issues?q=author:genialkartik+created:>2020-09-29+type:pr')
     const data = await response.json()
-    data.items.map(({ repository_url, html_url, title, labels, state, created_at }) => {
-      var _label = labels.find(e => e.name == 'hacktoberfest-accepted')
+    var t = data.items.length
+    var item = data.items;
+    console.log(t)
+    for (var i = 0; i < t; i++) {
+      var _label = item[i].labels.find(e => e.name == 'hacktoberfest-accepted')
       let label_bool = _label ? true : false
-      var cb = getTopics(repository_url)
-      console.log(cb)
+      const user_repo_res = await fetch(item[i].repository_url + '/topics',
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/vnd.github.mercy-preview+json",
+            Authorization: 'token ee8a590676c13ab98ed25693da8a0fd4281dc0cb'
+          }
+        })
+      const user_data = await user_repo_res.json()
+      var _topic = user_data.names.find(e => e == 'hacktoberfest')
+      let topic_bool = _topic ? true : false
+
       ar_PR.push({
-        title,
-        html_url,
-        repository_url,
-        cb,
+        title: item[i].title,
+        html_url: item[i].html_url,
+        repo_url: item[i].repository_url,
+        topic_bool,
         label_bool,
-        state,
-        created_at
+        state: item[i].state,
+        created_at: item[i].created_at
       })
-    })
+    }
     callback(ar_PR)
   } catch (error) {
     console.log('API error: ' + error)
@@ -65,7 +66,7 @@ async function getPRs(callback) {
   }
 }
 
-router.get('/:username', (req, res) => {
+router.get('/user/:username', (req, res) => {
   getPRs(cb => {
     res.send(cb)
   })
@@ -75,9 +76,4 @@ router.get('/:username', (req, res) => {
 
 
 module.exports = router;
-
-// Client ID
-//     85fb922b08a0b9bb0496
-// Client Secret
-//     2ebf230e6396c7fa226d304892ea7771c63ffce3
 
