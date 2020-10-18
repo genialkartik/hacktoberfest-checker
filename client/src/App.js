e2353a6f4e921acffb9d094007f6adc1053b847e
Ankur Jarial <ankujarialthakur@gmail.com>
2020-10-18 22:27:47 +0530

    Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route path="/" exact component={Home}></Route>
-      </Router>
+      <Home/>
     </div>
   );
 }
