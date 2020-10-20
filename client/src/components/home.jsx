5e12812bed011c8f9236d31f1a2112fe43d72787
NorinMp143 <madanparmar637@gmail.com>
2020-10-20 11:16:07 +0530

    inmproved styles + responsive design + add loader

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -13,9 +13,11 @@ function Home() {
   const [bool, Setbool] = useState(false);
   const [count, Setcount] = useState(1);
   const [message, Setmessage] = useState("");
+  const [loaderToggle, SetLoader] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    SetLoader(true);
     await axios.post("https://hacktoberfestcheck.herokuapp.com", { uname: username }).then((res => {
       if (res.status === 200) {
         var data = res.data;
@@ -43,6 +45,7 @@ function Home() {
           Setmessage("You need to do" + 4 - count + "PR's")
         }
         Setbool(true);
+        SetLoader(false);
       }
 
     }))
@@ -51,81 +54,89 @@ function Home() {
   return (
     <div>
       <Container fluid className={"nav center"}>
-        <Row >
+        <Row className={"container text-center"}>
           <Col ><img src={announcement} height="40px" alt="announcement" /> &nbsp;Don't forget to
           <a href="https://hacktoberfest.digitalocean.com/" target="_blank" rel="noopener noreferrer"> register</a>  to be eligible for the tee or tree!</Col>
         </Row>
       </Container>
 
       <div className={"main"}>
-        <div className={"center"}>
+        <div className={"center hacktoberfest-imgbox"}>
           <Image
-            src={require('./hack.jpg')}
-            height="258px"
-            width="700px"
+            src={require('./hack.svg')}
+            height="260px"
           />
         </div>
 
-        <h1 className={"center"} style={{ color: "#FF8AE2", fontFamily: "cursive" }}>Check Your Progress</h1>
+        <h1 className={"center text-center"} style={{ color: "#FF8AE2", fontFamily: "sans" }}>Check Your Progress</h1>
 
 
-        <Form onSubmit={handleSubmit} autocomplete="off" inline className={"justify-content-center form1"}>
-          <div style={{ margin: "0cm 2cm 0cm 3cm" }}>
-            <Image roundedCircle src="https://avatars1.githubusercontent.com/u/32240906?s=460&v=4" width="100px" height="100px" />
+        <Form onSubmit={handleSubmit} autocomplete="off" inline className={"row justify-content-center form1"}>
+          <div className={"col-12 col-sm-10 col-lg-8 d-flex"}>
+            <div className={"avatarBox"}>
+              <Image roundedCircle src="https://avatars1.githubusercontent.com/u/32240906?s=460&v=4" width="100px" height="100px" />
+            </div>
+            <div className="formBox row align-items-center justify-content-around">
+              <Form.Control
+                className={"col-12 col-sm-8 col-md-9 form"}
+                type="text"
+                placeholder="GitHub Username"
+                name="uname"
+                onChange={(e) => {
+                  setUsername(e.target.value)
+                }}
+              />
+              <Button className={"col-6 col-sm-3 col-md-2"} variant="outline-primary"
+                type="submit"
+              >Check</Button>
+            </div>
           </div>
-          <Form.Control
-            className={"form"}
-            type="text"
-            placeholder="GitHub Username"
-            name="uname"
-            onChange={(e) => {
-              setUsername(e.target.value)
-            }}
-          />
-          <Button variant="outline-primary"
-            type="submit"
-          >Check</Button>
         </Form>
 
 
-        <div className={"profile"}>
+        <div className={"profile"} style={ bool ? { padding:"10px" } : {} }>
           <div>
-            {bool ? <CircleProgress percentage={((count / 4) * 100)} width={80} /> : <p></p>}
-            <p>{message}</p>
+            {bool ? <CircleProgress percentage={((count / 4) * 100)} width={80} /> : ''}
+            { message !== '' ? <p>{message}</p> : '' }
           </div>
         </div>
 
-        <div>{bool ?
-          <div>
+        <div className={"container"}>{bool ?
+          <div className={"row justify-content-center"}>
             {data.map((d) => (
-              <div style={{ display: "flex " }}>
-                <Card className={"card-info"} style={d.label_bool || d.topic_bool ? { borderColor: "green" } : { borderColor: "Red" }}>
-                  <p style={{ fontSize: "3mm", margin: "10px 0px 0px 10px" }}>{new Date(Date.parse(d.created_at)).toUTCString()}</p>
-                  <p style={{ margin: "10px 0px 0px 10px" }}>You submitted
-                    <a href={d.pr_url} target="_blank" rel="noopener noreferrer" >{d.title}</a> to
-                    <a href={d.repo_url} target="_blank" rel="noopener noreferrer" >{d.repo_name}</a>
-                  </p>
-                </Card>
-                <Card className={"detail-card"} style={d.label_bool || d.topic_bool ? { borderColor: "green" } : { borderColor: "Red" }} >
-                  <p style={{ margin: "4px 0px 0px 10px", height: "33%" }}>
-                    Label/Topic : {d.label_bool || d.topic_bool ?
-                      <img src={require('./correct.png')} height="30px" width="30px" alt="topic" /> :
-                      <img src={require('./wrong.png')} height="30px" width="30px" alt="label" />}</p>
-                  <p style={{ margin: "0px 0px 5px 10px", height: "33%" }}>Pull Request Status : &nbsp;  {d.state}</p>
-                  <p style={{ margin: "0px 0px 5px 10px", height: "33%" }}>Public Repository : &nbsp;   <img src={require('./correct.png')} height="30px" width="30px" alt="topic" /> </p>
-                  <p style={{ margin: "0px 0px 5px 10px", height: "33%" }}>Review Period : {d.review}</p>
-                </Card>
+              <div className={"col-10 pullbox"}>
+                <div className={"row"}>
+                  <Card className={"col-12 col-sm-6 col-lg-7 card-info"}>
+                    <p style={{ fontSize: "3mm" }}>{new Date(Date.parse(d.created_at)).toUTCString()}</p>
+                    <p>You submitted 
+                      <a href={d.pr_url} target="_blank" rel="noopener noreferrer" > {d.title}</a> to 
+                      <a href={d.repo_url} target="_blank" rel="noopener noreferrer" > {d.repo_name} </a>
+                    </p>
+                  </Card>
+                  <Card className={"col-12 col-sm-6 col-lg-4 detail-card"} style={d.label_bool || d.topic_bool ? { borderColor: "#2ecc71",backgroundColor:"rgba(46,204,113,0.1)" } : { borderColor: "#e74c3c",backgroundColor:"rgba(231,76,60,0.1)" }} >
+                    <p>
+                      Label/Topic : {d.label_bool || d.topic_bool ?
+                        <img src={require('./correct.png')} height="30px" width="30px" alt="topic" /> :
+                        <img src={require('./wrong.png')} height="30px" width="30px" alt="label" />}</p>
+                    <p>Pull Request Status : &nbsp;  {d.state}</p>
+                    <p>Public Repository : &nbsp;   <img src={require('./correct.png')} height="30px" width="30px" alt="topic" /> </p>
+                    <p>Review Period : {d.review}</p>
+                  </Card>
+                </div>
               </div>
             ))}
           </div>
 
-          : <p> </p>
+          : ''
         }</div>
 
+        <div className="loader" style={loaderToggle?{ display:'block'} : {display:'none'}}>
+          <div className="loadscreen">
+          </div>
+        </div>
 
 
-
-        <footer className={"center"} style={{ marginTop: "50px" }}>
+        <footer className={"center"} style={{marginTop:bool?"50px":"20px",textAlign:'center' }}>
           <p>Attention : This site is just a fan made and it is not affiliated by <a href="https://hacktoberfest.digitalocean.com/" target="_blank" rel="noopener noreferrer">Hacktoberfest</a></p>
         </footer>
       </div>
@@ -134,3 +145,4 @@ function Home() {
   );
 }
 export default Home;
+
